71102995661fa417a3d28c831ea51ceb7978e5bd
Andreen <andreenshii@gmail.com>
2022-08-28 21:50:55 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+function ceo() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(resp => resp.json())
+      .then(json => renderImg(json))
+  }
+
+  function renderImg(images) {
+    const list = document.getElementById('dog-breeds');
+    const loc = document.querySelector('div')
+    images.message.forEach(Image => {
+      const img = document.createElement('img');
+      img.src = Image;
+      loc.appendChild(img)
+      list.appendChild(loc);
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    ceo()
+  })
